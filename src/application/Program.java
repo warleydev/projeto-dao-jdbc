f43eebc52cfb9ff5653797adea744bf3b7d71462
Warley Dev <warleycm7@gmail.com>
2023-03-30 16:34:31 -0300

    Implementando o método findById na classe SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,18 +1,16 @@
 package application;
 
-import java.util.Date;
-
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
-		Department obj = new Department(1, "Books");
-		Seller seller = new Seller(2012, "Warley", "warleycm7@gmail.com", new Date(), 3200.00, obj);
-		
+		//interface SellerDao sendo criada a partir da fábrica (DaoFactory)
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("------ TESTE 1: seller findById -----");
+		Seller seller = sellerDao.findById(3);
 		
 		System.out.println(seller);
 	}
