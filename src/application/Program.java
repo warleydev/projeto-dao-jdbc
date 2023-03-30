05c6f1bf1efe300dc7888ac64f3bf1add012a141
Warley Dev <warleycm7@gmail.com>
2023-03-30 15:33:36 -0300

    Adicionando a classe de entidade Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,14 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
 		Department obj = new Department(1, "Books");
-		System.out.println(obj);
+		Seller seller = new Seller(2012, "Warley", "warleycm7@gmail.com", new Date(), 3200.00, obj);
+		System.out.println(seller);
 	}
 }
