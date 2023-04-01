204559d5be1611ca14f870bf140583d966807c6c
Warley Dev <warleycm7@gmail.com>
2023-04-01 00:52:44 -0300

    Implementação do método deleteById na classe SellerDaoJDBC

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,8 +1,11 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
+import db.DbException;
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
@@ -10,6 +13,7 @@ import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		//interface SellerDao sendo criada a partir da fábrica (DaoFactory)
 		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
@@ -27,13 +31,17 @@ public class Program {
 		
 		System.out.println("\n------ TESTE 3: seller findAll -----");
 		
+		List<Integer> listId = new ArrayList<>();
+		
+		
 		list = sellerDao.findAll();
 		for (Seller obj : list) {
+			listId.add(obj.getId());
 			System.out.println(obj + "\n");
 		}
 		
 		System.out.println("\n------ TESTE 4: seller insert -----");
-		Seller newSeller = new Seller(null, "Orlando Torres", "orlandot@gmail.com", new Date(), 2300.00, department);
+		Seller newSeller = new Seller(2, "João Carlos", "bob@gmail.com", new Date(), 2300.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println(newSeller);
 		
@@ -42,5 +50,13 @@ public class Program {
 		seller.setName("Joao Carlos");
 		sellerDao.update(seller);
 		System.out.println(seller);
+		
+		System.out.println("\n------ TESTE 6: seller delete -----");
+		System.out.print("Digite o Id que queira deletar: ");
+		int id = sc.nextInt();
+		System.out.println("Deletando vendedor " + sellerDao.findById(id).getName() + "...");
+		sellerDao.deleteById(id);
+		System.out.println("Deletado com sucesso!");
+		sc.close();
 	}
 }
